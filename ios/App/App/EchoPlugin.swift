896848bcaab35788df69c2f4cce0c452b760ff7e
formicagreen <sr@yogameditation.com>
2023-11-30 11:38:40 +0100

    try to fix build issues

--- ios/App/App/EchoPlugin.swift
+++
@@ -1,9 +0,0 @@
-import Capacitor
-
-@objc(EchoPlugin)
-public class EchoPlugin: CAPPlugin {
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve(["value": value])
-    }
-}
